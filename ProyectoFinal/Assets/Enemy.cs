a5b4fabed892b54971ff438f84fac815ff334a4a
Luisdavidpm <alu0100816518@ull.edu.es>
2017-01-08 21:33:22 +0000

    Añadiendo primeras casa

---
+++ ProyectoFinal/Assets/Enemy.cs
@@ -0,0 +1,29 @@
+﻿﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	NavMeshAgent agent;
+
+	GameObject target;
+
+	void Awake(){
+
+		agent = GetComponent<NavMeshAgent>();
+
+		target = GameObject.FindGameObjectWithTag("Player");
+
+		agent.SetDestination(target.transform.position);
+
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
