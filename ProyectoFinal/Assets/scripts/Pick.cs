4ec1a999d7acb5f8119598285f2615d623e157b6
Luisdavidpm <alu0100816518@ull.edu.es>
2017-01-16 17:58:20 +0000

    Añadiendo llave e inventario llave

---
+++ ProyectoFinal/Assets/scripts/Pick.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pick : MonoBehaviour {
+	
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject.name == "Player"){
+			if (Input.GetKeyDown ("e")) {
+				Destroy (gameObject);
+				Destroy (GameObject.FindWithTag ("Vacio"));
+			}
+		}
+	}
+}
+
