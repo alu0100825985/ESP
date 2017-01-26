bc366d37d44cb6f6d53af658b36e8dd325a0d7c0
alu0100819182 <alu0100819182@ull.edu.es>
2017-01-26 22:19:54 +0000

    Añadido script Caida

---
+++ ProyectoFinal/Assets/scripts/Caida.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Caida : MonoBehaviour {
+
+	public GameObject cabeza1;
+	public GameObject cabeza2;
+	public Rigidbody rb;
+	public Rigidbody rb2;
+	// Use this for initialization
+	void Start () {
+		rb = cabeza1.GetComponent<Rigidbody>();
+		rb2 = cabeza2.GetComponent<Rigidbody>();
+	}
+
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.name == "Player") {
+			rb.useGravity = true;
+			rb2.useGravity = true;
+ 		}
+	}
+
+}
---
+++ ProyectoFinal/Assets/scripts/Llorar.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class Llorar : MonoBehaviour {
+	private AudioSource sonido;
+	public int stop = 0;
+	public AudioClip cryer;
+	public float vol = 1.0f;
+
+	void OnTriggerEnter(Collider other) {
+		if (stop == 0) {
+			sonido = GetComponent<AudioSource> ();
+			sonido.PlayOneShot (cryer, vol);
+			stop = 1;
+		}
+	}
+}
