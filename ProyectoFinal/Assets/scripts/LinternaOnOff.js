f63c42100696772946a1d8877ff95d2ab4edc6be
Luisdavidpm <alu0100816518@ull.edu.es>
2017-01-27 01:35:32 +0000

    Añadiendo script de screamer

---
+++ ProyectoFinal/Assets/scripts/LinternaOnOff.js
@@ -0,0 +1,14 @@
+﻿
+function Update () 
+{
+   if(Input.GetKeyDown("f"))
+   
+    if(GetComponent.<Light>().enabled == true)
+    
+       GetComponent.<Light>().enabled = false;
+       
+       else
+       
+       GetComponent.<Light>().enabled =  true;
+
+   }
---
+++ ProyectoFinal/Assets/scripts/Screamer.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+using UnityEngine.UI;
+
+public class Screamer : MonoBehaviour {
+
+	public GameObject gameOver;
+	public GameObject luz;
+	public GameObject llave;
+	private AudioSource sonido;
+	public int stop = 0;
+	public AudioClip grito;
+	public float vol = 1.0f;
+	float currentTime = 0f;
+	float step = 0.1f;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	IEnumerator TimerRoutine(){
+		while (currentTime < 1.5) {
+			yield return new WaitForSeconds (step);
+			currentTime += step;
+		}
+		gameOver.SetActive (false);
+		gameObject.SetActive (false);
+		luz.GetComponent<Light>().enabled = true;
+		llave.SetActive (true);
+
+	} 
+
+
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.name == "Player") {
+			gameOver.SetActive (true);
+			if (stop == 0) {
+				sonido = GetComponent<AudioSource> ();
+				sonido.PlayOneShot (grito, vol);
+				stop = 1;
+			}
+			StartCoroutine (TimerRoutine());
+		}
+	}
+}
