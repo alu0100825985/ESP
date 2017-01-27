8b165affe62230342921124f87112eeb0cc820c7
alu0100825985 <alu0100825985@ull.edu.es>
2017-01-27 06:12:01 +0000

    Terminado el juego con VR

---
+++ ProyectoFinal/Assets/scripts/AbrirVR.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AbrirVR : MonoBehaviour {
+
+	public void Open() {
+		if (Obtener.picked == 1) {
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ ProyectoFinal/Assets/scripts/PickVR.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickVR : MonoBehaviour {
+
+	public void Coger() {
+		Debug.Log ("Hola");
+		Obtener.picked = 1;
+		Destroy (gameObject);
+		Destroy (GameObject.FindWithTag ("Vacio"));
+	}
+}
